d1072fb54bcaa908bbf6c5a8b11e38f0ab70fc25
neil patel <neil@myindianweddingapp.com>
2017-08-25 10:52:01 +0100

    Added edit/update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
+
 
   def index
     @portfolio_items = Portfolio.all
@@ -8,21 +10,42 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.new
   end
 
+  def show
+  end
+
+  def edit
+  end
+
+
   def create
-     @portfolio_item = Portfolio.new(portfolio_params)
+    @portfolio_item = Portfolio.new(portfolio_params)
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to pages_home_path, notice: 'Your Portfolio was added successfully' }
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio item is now live' }
       else
         format.html { render :new }
       end
     end
   end
 
+  def update
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
   private
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
       params.require(:portfolio).permit(:title, :subtitle, :body)
-    end
+  end
 end
