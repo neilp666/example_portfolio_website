5b024e6990735271bd1423b7813c4ceaf363ba2c
neil patel <neil@myindianweddingapp.com>
2017-08-26 09:33:26 +0100

    updated controller and routes for pages

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,11 +40,17 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Destroy/delete the record
     @portfolio_item.destroy
+
+    # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolio_url, notice: 'Portfolio was removed.' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
-  end
+end
 
   private
 
