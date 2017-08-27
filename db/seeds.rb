2079e3cce752cd0928d38f8b0ad869c90e334e34
neil patel <neil@myindianweddingapp.com>
2017-08-27 15:45:23 +0100

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+3.times do |topic|
+    Topic.create!(title: "Topic #{topic}")
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
@@ -5,7 +11,7 @@
     since the 1500s is reproduced below for those interested. 
     Sections 1.10.32 and 1.10.33 from  by Cicero are also reproduced in their exact original form, accompanied 
     by English versions from the 1914
-    translation by H. Rackham."
+    translation by H. Rackham.", topic_id: Topic.last.id
     )
 end
 
@@ -20,10 +26,10 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
   Portfolio.create!(
     title: "Portfolio title: #{portfolio_item}",
-    subtitle: "My great service" ,
+    subtitle: "Ruby on Rails" ,
     body: "The standard chunk of Lorem Ipsum used 
     since the 1500s",
     main_image: "http://via.placeholder.com/600x400",
@@ -31,4 +37,14 @@ puts "5 skills created"
     )
 end
 
-puts "9 portfolio items created"
+1.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "Javascript" ,
+    body: "The standard chunk of Lorem Ipsum used 
+    since the 1500s",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+    )
+end
+
